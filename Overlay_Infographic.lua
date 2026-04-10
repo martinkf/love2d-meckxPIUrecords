9521b8a3f25af7dbc93ff9eece4d25673ae4220c
Martin Knop <martinkf@live.com>
2026-04-10 00:59:39 -0300

    yay for that overlay

--- Overlay_Infographic.lua
+++ Overlay_Infographic.lua
@@ -3,18 +3,13 @@ local Overlay_Infographic = {}
 function Overlay_Infographic.Drawing()
 	local drawingX = 4
 	local drawingY = 48
-	local linebreakSize = 26
+
 	local numberOfSongs = #DatabaseMixes[Game.selectedMixIndex].SortingMethods[Game.selectedSortIndex].AvailableSongs
 	local totalWidth = 1272
     local totalHeight = 668
-	local columnWidth = totalWidth / numberOfSongs
 
-	local colors = {
-        "black",
-        "gray",
-        "white",
-        "gray"
-    }
+	local cols = math.ceil(math.sqrt(numberOfSongs))
+    local rows = math.ceil(numberOfSongs / cols)
 
 	-- background
 	meckx_rect({
@@ -27,15 +22,22 @@ function Overlay_Infographic.Drawing()
 		Transparency = 1,
 	})
 
-	for i = 1, numberOfSongs do
-        local xPos = drawingX + (i - 1) * columnWidth
-        local color = colors[((i - 1) % #colors) + 1]
+	-- da grid
+	for i=1, numberOfSongs do
+        local col = (i - 1) % cols
+        local row = math.floor((i - 1) / cols)
+
+        local xPos = drawingX + (col * (totalWidth / cols))
+        local yPos = drawingY + (row * (totalHeight / rows))
+
+        local songTitle = DatabaseMixes[Game.selectedMixIndex].SortingMethods[Game.selectedSortIndex].AvailableSongs[i].SongTitle
+		local color = DatabaseDetails.FetchSongColorEnabled(songTitle)
 
         meckx_rect({
             XPos = xPos,
-            YPos = drawingY,
-            Width = columnWidth,
-            Height = totalHeight,
+            YPos = yPos,
+            Width = (totalWidth / cols),
+            Height = (totalHeight / rows),
             ColorName = color,
             RectStyle = "fill",
             Transparency = 1,
